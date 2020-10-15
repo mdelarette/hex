99f7f8ecd42cda88cba701bc92b8613d74fd7501
Matthias Delarette <mdelarette@foussier.fr>
2020-10-15 17:41:18 +0200

    no scroll bar, initial size

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import {useState, useEffect} from 'react';
 
 
 const Canvas: React.FC<{id:string, zIndex:number}> = ({id, zIndex}) => {
-    return <canvas id={id} width="200" height="150" style={{border:"1px solid #000000", position: "absolute", zIndex: zIndex}}/>
+
+    const [width, setWidth] = useState(200);
+    const [height, setHeight] = useState(150);
+
+    useEffect(() => {
+        console.log('Window initial size', window.innerWidth, window.innerHeight);
+        setWidth(window.innerWidth);
+        setHeight(window.innerHeight);
+    }, []);
+
+
+    return <canvas id={id} width={width} height={height} style={{border:"1px solid #000000", position: "absolute", zIndex: zIndex}}/>
 }
 
   
