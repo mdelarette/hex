48d6e07858987c82a41030ebe34aff1a1b1fe898
Matthias Delarette <mdelarette@foussier.fr>
2020-10-19 08:42:13 +0200

    Updates

---
+++ precache-manifest.be6fb3ac48accc0c01245d1ca7a8f221.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b254be9eb0e56dfbef3126281b5a9b4",
+    "url": "/hex/index.html"
+  },
+  {
+    "revision": "aadf752a917833598dc5",
+    "url": "/hex/static/css/main.d9bccc89.chunk.css"
+  },
+  {
+    "revision": "c86748b7ba1959b2d2b1",
+    "url": "/hex/static/js/2.06e17540.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/hex/static/js/2.06e17540.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aadf752a917833598dc5",
+    "url": "/hex/static/js/main.3943d34a.chunk.js"
+  },
+  {
+    "revision": "3a3d410bac452cfe5bcc",
+    "url": "/hex/static/js/runtime-main.904bb54f.js"
+  }
+]);
