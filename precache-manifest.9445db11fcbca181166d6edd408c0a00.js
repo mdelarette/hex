9558a6cc8147f19245bc9dfbbb4dc356979e254b
Matthias Delarette <mdelarette@foussier.fr>
2020-10-23 10:25:49 +0200

    Updates

--- precache-manifest.9445db11fcbca181166d6edd408c0a00.js
+++ precache-manifest.9445db11fcbca181166d6edd408c0a00.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2995e62d44bbbb9ccd4d82131e706625",
+    "revision": "4bf8d4ea7d4e8ad9262d34d5bcd73ed9",
     "url": "/hex/index.html"
   },
   {
-    "revision": "7c39dd034a6877ad11bb",
+    "revision": "4fe123c2c9dec92f9ef8",
     "url": "/hex/static/css/main.9111ac81.chunk.css"
   },
   {
-    "revision": "8dfa1c4219eab2315942",
-    "url": "/hex/static/js/2.56390ab1.chunk.js"
+    "revision": "f31a6babd2973db38a47",
+    "url": "/hex/static/js/2.fa9f907e.chunk.js"
   },
   {
     "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
-    "url": "/hex/static/js/2.56390ab1.chunk.js.LICENSE.txt"
+    "url": "/hex/static/js/2.fa9f907e.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "7c39dd034a6877ad11bb",
-    "url": "/hex/static/js/main.1978f488.chunk.js"
+    "revision": "4fe123c2c9dec92f9ef8",
+    "url": "/hex/static/js/main.45bf0f8b.chunk.js"
   },
   {
     "revision": "3a3d410bac452cfe5bcc",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/hex/precache-manifest.c4b6cfed7cada98db7193ce92e164f05.js"
+  "/hex/precache-manifest.9445db11fcbca181166d6edd408c0a00.js"
 );
 
 self.addEventListener('message', (event) => {
