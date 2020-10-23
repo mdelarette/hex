2e0aa67a8253005ff7db04de616f3799d50b8eda
Matthias Delarette <mdelarette@foussier.fr>
2020-10-23 10:20:27 +0200

    removed unused import

--- src/App.tsx
+++ src/App.tsx
@@ -11,11 +11,8 @@ import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
 
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
-import SearchIcon from '@material-ui/icons/Search';
-import MoreIcon from '@material-ui/icons/MoreVert';
 
 import TouchAppIcon from '@material-ui/icons/TouchAppTwoTone';
 import MouseIcon from '@material-ui/icons/MouseTwoTone';
